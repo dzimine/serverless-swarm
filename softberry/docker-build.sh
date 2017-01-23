05075bcfd5f39d42bd2ff3bcc18c2afdf5e8d5fc
Dmitri <dz@stackstorm.com>
2017-01-22 18:43:54 -0800

    Update docker build script.

--- softberry/docker-build.sh
+++ softberry/docker-build.sh
@@ -3,14 +3,18 @@
 
 set -e
 
+
 REGISTRY=st2.my.dev:5000
 
-docker build -t $REGISTRY/blast -f Dockerfile.blast .
-docker build -t $REGISTRY/sb_base -f Dockerfile.sb_base .
-docker build -t $REGISTRY/fgenesb -f Dockerfile.fgenesb .
-docker build -t $REGISTRY/blast_prep -f Dockerfile.blast_prep .
-docker build -t $REGISTRY/blast_fb -f Dockerfile.blast_fb .
-docker build -t $REGISTRY/fgenesb_out -f Dockerfile.fgenesb_out .
+# Order matters, so don't be too smart :)
+#    IMAGES=$(ls Dockerfile.* | awk -F . '{ print $NF }')
+IMAGES=( blast sb_base fgenesb blast_prep blast_fb fgenesb_out )
 
-# Push images to local registry
-# docker images | grep $REGISTRY | awk '{ system("docker push " $1) }'
+echo ${IMAGES[@]}
+
+for i in "${IMAGES[@]}"; do
+  echo "Build image for $i ..."
+  docker build -t sb/$i -t $REGISTRY/$i -f Dockerfile.$i .
+  echo "Pushing $i to local registry ..."
+  docker push $REGISTRY/$i
+done
