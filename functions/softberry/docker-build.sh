f1d844453112bcf55fd882d006e8d5c38bb5ecf9
Dmitri <dz@stackstorm.com>
2017-02-20 05:33:39 -0800

    Add ‘create graphics’ to findgenesb
    Also: 
    - rename dockerfiles for Sublime highligther
    - add names to workflow steps

--- functions/softberry/docker-build.sh
+++ functions/softberry/docker-build.sh
@@ -8,13 +8,19 @@ REGISTRY=st2.my.dev:5000
 
 # Order matters, so don't be too smart :)
 #    IMAGES=$(ls Dockerfile.* | awk -F . '{ print $NF }')
-IMAGES=( blast sb_base fgenesb blast_fb fgenesb_out )
+IMAGES=( blast_base sb_base fgenesb blast_fb fgenesb_out )
 
 echo ${IMAGES[@]}
 
 for i in "${IMAGES[@]}"; do
   echo "Build image for $i ..."
-  docker build -t sb/$i -t $REGISTRY/$i -f Dockerfile.$i .
+  docker build -t sb/$i -t $REGISTRY/$i -f $i.Dockerfile .
   echo "Pushing $i to local registry ..."
   docker push $REGISTRY/$i
 done
+
+echo "Building image for cgview ..."
+docker build -t sb/cgview -t $REGISTRY/cgview -f ./FGenesb_CGView/Dockerfile FGenesb_CGView/
+echo "Building cgview to local registry ..."
+docker push $REGISTRY/cgview
+
