f88d1086c43877312805044655011ebbfc951a72
Dmitri Zimine <dz@stackstorm.com>
2017-03-16 16:25:49 -0700

    Add route53 and manager (for real).

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,9 +20,17 @@ variable "gs_ebs_snapshot" {
 variable ami {
   default = "ami-a58d0dc5"
 }
-variable instance_type {
+variable instance_type_manager {
   default = "t2.micro"
+  # Recommened: t2.medium for testing, m4.large for production
+  # default = "t2.medium"
 }
+variable instance_type_worker {
+  default = "t2.micro"
+  # Recommened: t2.medium for testing, c4.xlarge for production
+  # default = "t2.medium"
+}
+
 variable subnet {
   default = "subnet-f2e732bb"
 }
@@ -31,4 +39,7 @@ variable "security_group_vpc" {
 }
 variable "security_group_ssh" {
   default = "sg-4f83f437"
+}
+variable "security_group_st2" {
+  default = "sg-cfdfa8b7"
 }
